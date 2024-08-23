f7a0dd6085d9a940adc423563f67ebbe3d0181a8
zewenn <takaczsoli@gmail.com>
2024-08-23 11:58:55 +0200

    v0.1.0

---
+++ generator/__init__.py
@@ -0,0 +1,54 @@
+from typing import *
+from . import resources
+import copy
+
+T = TypeVar("T", str, list[str])
+DSET = dict[int, T]
+void = type[None]
+
+DATASET: DSET | None = None
+
+
+def use_dataset(dataset: DSET) -> void:
+    global DATASET
+    DATASET = dataset
+
+
+def replace_keyword_keycode(template: resources.Template, kword: str, kcode: int) -> str:
+    fkw = kword
+    
+    if (not kword.isalpha()):
+        if (len(kword) == 1):
+            fkw = resources.BACKWARDS_TABLE[ord(kword)]
+    
+    return template.template.replace(f"${fkw},", f"{kcode},")
+
+
+def generate(name: str) -> resources.Template:
+    template: resources.Template = resources.get_template(name)
+    kwords_remaining = copy.deepcopy(resources.KEYWORDS)
+
+
+    for kcode, kword in DATASET.items():
+        keywords = kword if isinstance(kword, list) else [kword]
+
+        for keyword in keywords:
+            if (keyword not in kwords_remaining):
+                continue
+            
+            print(f"Replacing [${keyword}] with [{kcode}]")
+            template.template = replace_keyword_keycode(
+                template,
+                keyword,
+                kcode,
+            )
+            
+    for kword in kwords_remaining:
+        template.template = replace_keyword_keycode(
+            template,
+            kword,
+            "null"
+        )
+    
+    return template
+
---
+++ out/out.zig
@@ -0,0 +1,4 @@
+const y = enum(?u8) {
+    a = null,
+    b = 10
+};
